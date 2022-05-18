b5dd8e0e386fa10f83155b1e8991eb456a20c812
Nacho Contreras <nc.devw@gmail.com>
2022-05-18 12:53:43 -0300

    created hook rest client

---
+++ hooks/useRestClient.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+axios.defaults.baseURL = '/api/';
+
+const useRestClient = (axiosParams: AxiosRequestConfig) => {
+  const [response, setResponse] = useState<AxiosResponse>();
+  const [error, setError] = useState<AxiosError>();
+  const [loading, setLoading] = useState(true);
+
+ const fetchData = async (params: AxiosRequestConfig) => {
+    try {
+      const result = await axios.request(params);
+      setResponse(result);
+    } catch( err: any ) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+ };
+
+useEffect(() => {
+  fetchData(axiosParams);
+},[]);
+
+ return { response, error, loading };
+}
+
+export default useRestClient
