0e95f823c1f3a0177f6ad31d730ef9a5acafdabf
Nacho Contreras <nc.devw@gmail.com>
2022-05-18 10:51:48 -0300

    added links in header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import UserProfile from "../UserProfile";
 
+import Link from "next/link";
+
 const Header = () => {
   return (
     <div className="bg-blue-600 py-4 px-12">
       <div className="flex text-white font-semibold items-center">
         <div className="flex-[1]">Exchange App</div>
         <div className="flex-[4] flex gap-5">
-          <div>Home</div>
-          <div>Swap</div>
-          <div>Swap Status</div>
+          <Link href="/">Home</Link>
+          <Link href="/swap">Swap</Link>
+          <Link href="/swap-status">Swap Status</Link>
         </div>
         <div className="flex-[1] justify-end">
           <UserProfile />
