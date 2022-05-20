27506949102869ea587d4ddab99ec45face4da25
Nacho Contreras <nc.devw@gmail.com>
2022-05-20 19:32:20 -0300

    added context

---
+++ context/global.tsx
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+
+interface AppContext {
+  user?: User;
+}
+
+interface User {
+  name: string;
+  coins: Coin[];
+}
+
+export interface Coin {
+  symbol: string;
+  quantity: number;
+}
+
+// context.js
+export const Ctx = React.createContext<AppContext>({});
+
+export function useAppContext() {
+  return useContext(Ctx);
+}
+
+export function AppProvider({ children }: { children: React.ReactElement }) {
+  const [app, setApp] = React.useState<AppContext>({});
+
+  React.useEffect(() => {
+    setApp({
+      user: {
+        name: "ncdevw",
+        coins: [
+          {
+            symbol: "btc",
+            quantity: 0.002,
+          },
+        ],
+      },
+    });
+  }, []);
+
+  return <Ctx.Provider value={app}>{children}</Ctx.Provider>;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
+import { AppProvider } from "../context/global";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
+    <AppProvider>
+      <>
+        <Header />
+        <Component {...pageProps} />
+      </>
+    </AppProvider>
   );
 }
 
