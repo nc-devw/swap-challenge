43e888d2b070b48afa91a9ddbeffa5131947756d
Nacho Contreras <nc.devw@gmail.com>
2022-05-21 13:35:10 -0300

    added more icons

---
+++ public/assets/icons/ArrowDown.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ArrowDown = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      viewBox="0 0 50 50"
+    >
+      <path d="M24 40 8 24 10.1 21.9 22.5 34.3V8H25.5V34.3L37.9 21.9L40 24Z" />
+    </svg>
+  );
+};
+
+export default ArrowDown;
---
+++ public/assets/icons/ExpandMore.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpandMore = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      viewBox="0 0 50 50"
+    >
+      <path d="M24 30.75 12 18.75 14.15 16.6 24 26.5 33.85 16.65 36 18.8Z" />
+    </svg>
+  );
+};
+
+export default ExpandMore;
