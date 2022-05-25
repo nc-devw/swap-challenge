09029fac7d52e7318282510c8fe409aa589aeeeb
Nacho Contreras <nc.devw@gmail.com>
2022-05-25 09:52:08 -0300

    added select coin component

---
+++ components/TableSwap/SelectCoin.tsx
@@ -0,0 +1,118 @@
+import React, { ReactEventHandler } from "react";
+import Image from "next/image";
+import { CoinsBalance } from ".";
+import ExpandMore from "../../public/assets/icons/ExpandMore";
+
+const SelectCoin = ({
+  coins,
+  currency,
+  setCurrency,
+  value,
+  setValue,
+  swapCurrency,
+  setSwapValue,
+  name,
+}: {
+  coins: CoinsBalance[];
+  currency: CoinsBalance;
+  setCurrency: React.Dispatch<React.SetStateAction<CoinsBalance>>;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  swapCurrency: CoinsBalance;
+  setSwapValue: React.Dispatch<React.SetStateAction<string>>;
+  name?: string;
+}) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleCurrency = (coin: CoinsBalance) => {
+    setCurrency(coin);
+    setOpen(false);
+  };
+
+  const handleChange = (
+    ev: React.FormEvent<HTMLInputElement>,
+    maxValue: number
+  ) => {
+    const currentValue = ev.currentTarget.value;
+
+    if (currentValue === "") {
+      setValue(currentValue);
+    }
+    if (!/^\d+\.{0,1}\d{0,15}$/.test(currentValue)) {
+      return;
+    }
+
+    let newValue = "";
+
+    if (Number(currentValue) > maxValue && name !== "output") {
+      newValue = maxValue.toString();
+    } else {
+      newValue = ev.currentTarget.value;
+    }
+
+    const swapValue =
+      (Number(newValue) * currency.usdPrice) / swapCurrency.usdPrice;
+    setSwapValue(swapValue.toString());
+    return setValue(newValue);
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex relative">
+        <div
+          className="flex cursor-pointer"
+          onClick={() => {
+            setOpen((prevState) => !prevState);
+          }}
+        >
+          <Image
+            alt="cripto"
+            src={currency?.iconUrl || "/"}
+            width={20}
+            height={20}
+          />
+          <span className="ml-2 uppercase font-bold">{currency?.symbol}</span>
+          <ExpandMore className="h-6 w-6" />
+        </div>
+        <div className="pl-6 text-sm text-gray-400">
+          <span>Balance: {currency?.currentQuantity}</span>
+        </div>
+        <div
+          style={{ display: open ? "block" : "none" }}
+          className="shadow rounded absolute bg-white z-10 left-20 top-2 w-24 border"
+        >
+          {coins.map(
+            (coin: CoinsBalance) =>
+              coin.symbol !== currency?.symbol && (
+                <div
+                  className="flex items-center p-2 m-2 hover:text-slate-50 hover:bg-violet-500 cursor-pointer rounded"
+                  key={coin.symbol}
+                  onClick={() => handleCurrency(coin)}
+                >
+                  <Image
+                    alt="cripto"
+                    src={coin.iconUrl}
+                    width={20}
+                    height={20}
+                  />
+                  <span className="ml-2 font-bold uppercase text-sm">
+                    {coin.symbol}
+                  </span>
+                </div>
+              )
+          )}
+        </div>
+      </div>
+      <div className="m-2">
+        <input
+          className="bg-gray-100 rounded p-2"
+          type="text"
+          value={value}
+          onChange={(ev) => handleChange(ev, currency?.currentQuantity)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SelectCoin;
--- components/TableSwap/index.tsx
+++ components/TableSwap/index.tsx
@@ -1,48 +1,56 @@
 import { AxiosResponse } from "axios";
+import { useRouter } from "next/router";
 import React from "react";
 import { useAppContext } from "../../context/global";
 import useRestClient from "../../hooks/useRestClient";
+import SwapIcon from "../../public/assets/icons/Swap";
 import { Coin } from "../../services/transforms/coins";
 import Card from "../Card";
+import SelectCoin from "./SelectCoin";
 
 type Props = {};
 
 export interface CoinsBalance extends Coin {
   currentQuantity: number;
-  currentUsdPrice?: number;
+  currentUsdPrice: number;
 }
 
+const classDisabled = "cursor-not-allowed bg-gray-400 hover:bg-gray-600";
+
 const TableSwap = (props: Props) => {
+  const router = useRouter();
+
   const { response }: { response: AxiosResponse<Coin[], any> | undefined } =
     useRestClient({
       method: "GET",
       url: `/coins`,
     });
 
-  const { user } = useAppContext();
+  const { assets } = useAppContext();
 
-  const [balance, setBalance] = React.useState(0);
   const [coins, setCoins] = React.useState<CoinsBalance[]>([]);
 
   React.useEffect(() => {
     if (
       response?.data.length === 0 ||
-      user?.coins.length === 0 ||
+      assets.length === 0 ||
       coins.length !== 0
     ) {
       return;
     }
 
     response?.data.forEach((coinData) => {
-      const coin = user?.coins?.find((coin) => coin.symbol === coinData.symbol);
+      const coin = assets?.find((coin) => coin.symbol === coinData.symbol);
 
       if (!coin) {
-        let newArray = [...coins, { ...coinData, currentQuantity: 0 }];
+        let newArray = [
+          ...coins,
+          { ...coinData, currentQuantity: 0, currentUsdPrice: 0 },
+        ];
         setCoins(newArray);
         return;
       }
       const currentUsdPrice = coin.quantity * coinData.usdPrice;
-      setBalance((prevState) => prevState + currentUsdPrice);
 
       setCoins((prevState) => [
         ...prevState,
@@ -57,9 +65,46 @@ const TableSwap = (props: Props) => {
     });
 
     return;
-  }, [response, user]);
+  }, [response, assets]);
+
+  const [currencyInput, setCurrencyInput] = React.useState(coins[0]);
+  const [currencyOutput, setCurrencyOutput] = React.useState(coins[1]);
+
+  const [valueInput, setValueInput] = React.useState("0");
+  const [valueOutput, setValueOutput] = React.useState("0");
+
+  React.useEffect(() => {
+    setCurrencyInput(coins[0]);
+    setCurrencyOutput(coins[1]);
+  }, [coins]);
+
+  const isEnabled = React.useMemo(() => {
+    if (!Number(valueInput) || !Number(valueOutput)) {
+      return false;
+    }
+    if (
+      Number(valueInput) > currencyInput.currentQuantity ||
+      Number(valueOutput) > currencyOutput.currentQuantity
+    ) {
+      return false;
+    }
+
+    return true;
+  }, [valueInput, currencyInput, valueOutput, currencyOutput]);
+  const swapCurrencies = () => {
+    const oldCurrencyInput = { ...currencyInput };
+    const oldValueInput = valueInput;
+    setCurrencyInput(currencyOutput);
+    setValueInput(valueOutput);
+    setCurrencyOutput(oldCurrencyInput);
+    setValueOutput(oldValueInput);
+  };
+  console.log("coins", coins);
+  console.log("currencyInput", currencyInput);
+  console.log("currencyOutput", currencyOutput);
+
   return (
-    <Card>
+    <Card className="w-80">
       <div className="flex flex-col">
         <div className="text-center p-4 border-b">
           <p className="uppercase font-bold text-lg">Swap</p>
@@ -67,35 +112,52 @@ const TableSwap = (props: Props) => {
             Trade tokens in an instant
           </p>
         </div>
-        <div className="flex flex-col border-b p-4">
-          <div className="flex justify-between">
+        <div className="flex flex-col border-b p-4 items-center">
+          <div className="flex">
             <div className="flex">
-              {/* // select */}
-              <span>icon</span>
-              <span>BNB</span>
-              <span>chevron</span>
-            </div>
-            <div>
-              <span>Balance: 0.0017</span>
+              <SelectCoin
+                coins={coins}
+                currency={currencyInput}
+                setCurrency={setCurrencyInput}
+                value={valueInput}
+                setValue={setValueInput}
+                swapCurrency={currencyOutput}
+                setSwapValue={setValueOutput}
+              />
             </div>
           </div>
-          <div>
-            <span>chevron de swap</span>
+          <div className="pt-2 pb-3">
+            <SwapIcon
+              className="w-8 h-8 fill-violet-500 hover:fill-violet-600 cursor-pointer"
+              onClick={swapCurrencies}
+            />
           </div>
-          <div className="flex justify-between">
+          <div className="flex">
             <div className="flex">
-              {/* // select */}
-              <span>icon</span>
-              <span>BTC</span>
-              <span>chevron</span>
-            </div>
-            <div>
-              <span>Balance: 0.0017</span>
+              <SelectCoin
+                coins={coins}
+                currency={currencyOutput}
+                setCurrency={setCurrencyOutput}
+                value={valueOutput}
+                setValue={setValueOutput}
+                swapCurrency={currencyInput}
+                setSwapValue={setValueInput}
+                name="output"
+              />
             </div>
           </div>
         </div>
         <div className="text-center p-4">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button
+            className={`text-white font-bold py-2 px-4 rounded ${
+              isEnabled ? "bg-blue-500 hover:bg-blue-700 " : classDisabled
+            }`}
+            onClick={() => {
+              if (isEnabled) {
+                router.push("/confirmation");
+              }
+            }}
+          >
             Swap
           </button>
         </div>
