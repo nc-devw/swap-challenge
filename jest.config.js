af15d5b49e590c301c71a2f6e4286adb65bcc608
Nacho Contreras <nc.devw@gmail.com>
2022-05-29 20:00:16 -0300

    fixed aliases import

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,16 @@ const createJestConfig = nextJest({
 });
 
 const customJestConfig = {
+  moduleDirectories: ["node_modules", "src"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     "^@/components/(.*)$": "<rootDir>/components/$1",
     "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    "^@/context/(.*)$": "<rootDir>/context/$1",
+    "^@/helpers/(.*)$": "<rootDir>/helpers/$1",
+    "^@/public/(.*)$": "<rootDir>/public/$1",
+    "^@/services/(.*)$": "<rootDir>/services/$1",
+    "^@/styles/(.*)$": "<rootDir>/styles/$1",
   },
   testEnvironment: "jest-environment-jsdom",
   collectCoverageFrom: [
