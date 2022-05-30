a5fbce0263ab72ecfd134169a520d51846c5fdfb
Nacho Contreras <nc.devw@gmail.com>
2022-05-30 19:49:28 -0300

    added message for empty transactions

--- components/TransactionsCard/index.tsx
+++ components/TransactionsCard/index.tsx
@@ -13,28 +13,34 @@ const TransactionsCard = () => {
         <div className="text-center p-4 border-b">
           <p className="uppercase font-bold text-lg">Transactions</p>
         </div>
-        {transactions.map((transaction: Transaction) => (
-          <div
-            className="flex flex-col p-4 items-center justify-around border-b"
-            key={transaction.input.symbol}
-          >
-            <span className="font-bold">{transaction.date_created}</span>
-            <Detail
-              iconUrl={transaction.input.iconUrl}
-              symbol={transaction.input.symbol}
-              quantity={transaction.input.quantity}
-            />
-            <div className="flex items-center rotate-90 md:rotate-0">
-              <ChevronRight className="h-12 w-12" />
+        {transactions.length > 0 ? (
+          transactions.map((transaction: Transaction) => (
+            <div
+              className="flex flex-col p-4 items-center justify-around border-b"
+              key={transaction.input.symbol}
+            >
+              <span className="font-bold">{transaction.date_created}</span>
+              <Detail
+                iconUrl={transaction.input.iconUrl}
+                symbol={transaction.input.symbol}
+                quantity={transaction.input.quantity}
+              />
+              <div className="flex items-center rotate-90 md:rotate-0">
+                <ChevronRight className="h-12 w-12" />
+              </div>
+              <Detail
+                iconUrl={transaction.output.iconUrl}
+                symbol={transaction.output.symbol}
+                quantity={transaction.output.quantity}
+                output
+              />
             </div>
-            <Detail
-              iconUrl={transaction.output.iconUrl}
-              symbol={transaction.output.symbol}
-              quantity={transaction.output.quantity}
-              output
-            />
+          ))
+        ) : (
+          <div className="flex flex-col p-4 items-center justify-around border-b">
+            <span>There are no transactions</span>
           </div>
-        ))}
+        )}
       </div>
     </Card>
   );
